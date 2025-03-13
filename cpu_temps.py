ede037a4a613a762180950e7eaef90d99ebf3781
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2025-03-13 09:07:11 -0400

    Add code from recored lecture

--- cpu_temps.py
+++ cpu_temps.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python3
-
 import os
 import pprint as pp
 import sys
@@ -9,6 +7,7 @@ from typing import Iterable, Self
 import numpy as np
 
 import interpolation
+import least_squares
 from parse_temps import parse_raw_temps
 
 # Note: the type keyword requires Python 3.12 or newer
@@ -34,12 +33,21 @@ def write_core_file(
     core_filename = f"{base_name}-core-{core_idx}.txt"
 
     with open(core_filename, "w") as core_file:
-        # TODO: Least Squares Approximation output
         for time, time_next, y_int, slope in zip(*line_data):
             core_file.write(
                 f"{time:>6} <= x <= {time_next:>6} ;"
                 f"y = {y_int:>8.4f} + {slope:>8.4f} x ; interpolation\n"
             )
+
+        # TODO: Least Squares Approximation output
+        time = 0
+        time_next = 0
+        y_int = 0
+        slope = 0
+        core_file.write(
+            f"{time:>6} <= x <= {time_next:>6} ;"
+            f"y = {y_int:>8.4f} + {slope:>8.4f} x ; least-squares\n"
+        )
 
 
 def main():
@@ -59,7 +67,16 @@ def main():
     # Handle input and preprocssing
     # ---------------------------------------------------------------------------
     with open(temperature_filename, "r") as temps_file:
-        _ = parse_raw_temps(temps_file)
+        raw_temperature_data = parse_raw_temps(temps_file)
+
+        times = []
+        core_temps = [[] for _ in range(0, 4)]
+
+        for time, readings in raw_temperature_data:
+            times.append(time)
+
+            for core_idx, core_reading in enumerate(readings):
+                core_temps[core_idx].append(core_reading)
 
     # ---------------------------------------------------------------------------
     # Process each core and output the results
@@ -67,16 +84,24 @@ def main():
     # Remove the file extension (get everything up to the '.')
     base_filename = os.path.splitext(temperature_filename)[0]
 
+    pp.pprint(times)
+    times_lower = times[0:-1]
+    times_upper = times[1:]
     for core_idx, core_temps in enumerate(core_temps):
         # interpolation
         raw_line_data = interpolation.piecewise_linear(times, core_temps)
 
-        times_lower, times_upper, y_ints, slopes = raw_line_data
+        y_ints = []
+        slopes = []
+        for _, _, y_int, slope in raw_line_data:
+            y_ints.append(y_int)
+            slopes.append(slope)
+
+        # TODO: least squares approximation
 
         write_core_file(
             base_filename, core_idx, (times_lower, times_upper, y_ints, slopes)
         )
-        # TODO: least squares approximation
 
 
 if __name__ == "__main__":
--- interpolation.py
+++ interpolation.py
@@ -8,9 +8,26 @@ where ``{...}`` represents one or more functions.
 from typing import Generator
 
 
+def linear(
+    time_start, time_end, reading_start, reading_end
+) -> tuple[float, float]:
+    """
+    Compute the line between time_start and time end
+
+    Returns:
+
+        y-intercept and slope in the form of a tuple
+    """
+
+    slope = (reading_end - reading_start) / (time_end - time_start)
+    y_int = reading_end - (slope * time_end)
+
+    return (y_int, slope)
+
+
 def piecewise_linear(
     times: list[float], temperatures: list[float]
-) -> Generator[tuple[int, int, float, float], None, None]:
+) -> Generator[tuple[float, float, float, float], None, None]:
     """
     T.B.W.
 
@@ -20,4 +37,11 @@ def piecewise_linear(
         (start time, end time, y-intercept, slope)
     """
 
-    raise NotImplementedError()
+    reading_generator = zip(
+        times[0:-1], times[1:], temperatures[0:-1], temperatures[1:]
+    )
+
+    for start_time, end_time, start_reading, end_reading in reading_generator:
+        y_int, slope = linear(start_time, end_time, start_reading, end_reading)
+
+        yield start_time, end_time, y_int, slope
